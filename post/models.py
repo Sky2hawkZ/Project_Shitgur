4d1b8b5bc2a70c84424cfdae9ce8e597754b8263
Klovern <oskar.kindeland@gmail.com>
2016-05-17 13:08:03 +0200

    Detail
    bootstrap

--- post/models.py
+++ post/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     post_dislikes = models.IntegerField(default=0)
     post_image = models.FileField(upload_to='files/images')
     post_tags = models.CharField(max_length=1000, default=None)
+    post_user = models.CharField(max_length=40, default='Anonymus')
 
     def __str__(self):
         return self.post_text
