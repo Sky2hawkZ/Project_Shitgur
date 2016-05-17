fb3c2226118d27488ad15cda03333c19f0141823
Klovern <oskar.kindeland@gmail.com>
2016-05-17 11:37:08 +0200

    detail

--- post/views.py
+++ post/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.shortcuts import render, Http404
+from django.db import models
+from django.http import HttpResponse
+from models import Post
 
-# Create your views here.
+
+def detail(request, post_id):
+    try:
+        p = Post.objects.get(pk=post_id)
+    except p.DoesNotExist:
+        raise Http404("Post does not exist")
+    return render(request, 'post/detail.html', {'post': p})
+
