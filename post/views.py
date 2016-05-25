1c5f94ce5c4e6008ff2beeb2a2b3ae5aeeb027b1
Klovern <oskar.kindeland@gmail.com>
2016-05-25 10:30:39 +0200

    detail
    kommentarsfält frontend

--- post/views.py
+++ post/views.py
@@ -3,13 +3,16 @@ from django.views.generic.edit import CreateView
 from django.shortcuts import render, Http404
 from django.db import models
 from django.http import HttpResponse
-from models import Post
+from models import Post, Comment
 
 
 def detail(request, post_id):
     try:
         p = Post.objects.get(pk=post_id)
+        c = Comment.objects.get(comment_post=p)
+        p.post_views = getattr(p, 'post_views') +1;
+        p.save()
     except p.DoesNotExist:
         raise Http404("Post does not exist")
-    return render(request, 'post/detail.html', {'post': p})
+    return render(request, 'post/detail.html', {'post': p, 'comment': c})
 
