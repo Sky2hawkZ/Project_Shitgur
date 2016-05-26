c41e7a53939cf2e6d805a72743c04ac1c72fe66a
Sky2hawkZ <dan.lofgrens@gmail.com>
2016-05-26 13:02:06 +0200

    Settings "fixed"

--- Project_Shitgur/settings.py
+++ Project_Shitgur/settings.py
@@ -124,10 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = "/Github/Project_Shitgur/static/"
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    "post/static/post/",
-    os.path.join(BASE_DIR, "static"),
 
-)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
