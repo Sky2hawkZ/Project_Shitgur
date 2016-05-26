7e2fe5552b0fab8fe0085347975436a258ed25dd
Klovern <oskar.kindeland@gmail.com>
2016-05-26 14:01:34 +0200

    extened pk

--- post/models.py
+++ post/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
 
@@ -13,7 +16,7 @@ class Post(models.Model):
     post_favorited = models.IntegerField(default=0)
     post_image = models.ImageField()
     post_tags = models.CharField(max_length=1000, default=None)
-    post_user = models.CharField(max_length=40, default='Anonymus')
+    post_user = models.ForeignKey(User, on_delete=models.CASCADE)
     post_IsAdvertised = models.BooleanField(default='false')
 
     def __str__(self):
@@ -23,12 +26,14 @@ class Post(models.Model):
 class Comment(models.Model):
     comment_posted = models.DateTimeField('DateTime published')
     comment_text = models.TextField(max_length=2000)
-    comment_views = models.IntegerField(default=0)
     comment_favorited = models.IntegerField(default=0)
     comment_points = models.IntegerField(default=0)
-    comment_user = models.CharField(max_length=40, default='Anonymus')
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comment_text
+
+
+
 
