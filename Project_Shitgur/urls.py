2a8c744e0762d575153ce5c78319f3917b841c2d
Sky2hawkZ <dan.lofgrens@gmail.com>
2016-05-26 15:42:12 +0200

    fixes in .gitignore

--- Project_Shitgur/urls.py
+++ Project_Shitgur/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
