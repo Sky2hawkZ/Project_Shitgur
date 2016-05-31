110439eea9baf39acf4a9a1694ccc63af0b0b67f
ve2323 <kim.sillanpaa@hotmail.com>
2016-05-31 10:57:19 -0700

    stuffs
    - working registration
    - working login
    - working update password feature
    - working update user details feature
    - working display profile data feature
    - only logged in users will have access to account management
    - if user is already logged in and tries to create new account user will
    be sent to management page directly
    
    x forgot password tab within logreg does not work
    x favorites within account management does not contain data
    x user is not sent to account management when clicking login button on
    logreg
    x unused imports and code present

--- Project_Shitgur/settings.py
+++ Project_Shitgur/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     "post/static/post/",
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- account/views.py
+++ account/views.py
@@ -1,65 +1,34 @@
 from django.shortcuts import render
-from django.shortcuts import redirect 
-from django.http import HttpResponseRedirect 
-from django.contrib.auth.forms import UserCreationForm 
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth
 from account.models import user_data
 from forms import UserDataForm
+from forms import UserForm
 
-template_acc_name = 'account/Account_management.html'
-template_acc_error_name = 'account/Account_management_error.html'
-template_acc_success_name = 'account/Account_management_success.html'
+# Note to people messing around with the code: 
 
-template_logreg_name = 'account/reg_log-in_page.html'
-template_logreg_error_name = 'account/reg_log-in_error.html'
-template_logreg_success_name = 'account/reg_log-in_success.html'
-template_logreg_updatepass_name = 'account/reg_log-in_updatepass.html'
+# in older version Management within authview/register was named 'management'
+# accmanage was renamed to accmanage_regauth along with the url name change from management to Management
 
+# variables to html pages have been altered aswell
+
+# File locations set as variables due to frequent usage
+account_management = 'account/Account_management.html'
+account_error = 'account/Account_management_error.html'
+account_success = 'account/Account_management_success.html'
+
+logreg = 'account/reg_log-in_page.html'
+logreg_error = 'account/reg_log-in_error.html'
+logreg_success = 'account/reg_log-in_success.html'
+logreg_updatepass = 'account/reg_log-in_updatepass.html'
+
+# Render account login/registration page on request
 def acclogreg(request):
-    return render(request, template_logreg_name)
+    return render(request, logreg)
 
-@login_required
-def accmanage(request):
-    return render(request, template_acc_name)
-
-def authview(request):
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
-    user = auth.authenticate(username = username, password = password)
-    if user:
-        auth.login(request, user)
-        return redirect('management')
-    else:
-        return render(request, template_logreg_error_name)
-
-"""work in progress below this line"""
-
-def register(request):
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
-    password_repeat = request.POST.get('password_repeat', '')
-    email = request.POST.get('email', '')
-    dob = request.POST.get('dob', '')
-
-    if request.user.is_authenticated():
-        return redirect('management')
-    else:
-        if not User.objects.filter(username=username).exists() and username != "":
-            user = User(username=username)
-            User.email = email
-            user_data.date_of_birth = dob
-            if password == password_repeat and password != "":
-                user.set_password(password)
-            else:
-                return render(request, template_logreg_error_name)
-            user.is_active = True
-            user.save()
-            return render(request, template_logreg_success_name)
-        else:
-            return render(request, template_logreg_error_name)
-
+# Update user password and update page
 @login_required
 def update_password(request):
     current_password = request.POST.get('current_password', '')
@@ -71,44 +40,81 @@ def update_password(request):
         if new_password == repeat_password:
             request.user.set_password(new_password)
             request.user.save()
-            return render(request, template_logreg_updatepass_name)
+            return render(request, logreg_updatepass)
         else:
-            return render(request, template_acc_error_name)
+            return render(request, account_error)
     else:
-        return render(request, template_acc_error_name)
+        return render(request, account_error)
 
+# Update user profile and update page
 @login_required
 def update_user_info(request):
-    first_name = request.POST.get('first_name', '')
-    last_name = request.POST.get('last_name', '')
-    gender = request.POST.get('gender', '')
-    country = request.POST.get('country', '')
-    date_of_birth = request.POST.get('date_of_birth', '')
-    about = request.POST.get('about', '')
+    user_form = UserForm(request.POST or None, request.FILES, instance=request.user)
+    user_data_form = UserDataForm(request.POST or None, request.FILES, instance=request.user.user_data)
 
-    if request.POST:
-        form = UserDataForm(request.POST or None)
-        if form.is_valid():
-            user = form.save()
+    if user_data_form.is_valid() and user_form.is_valid():
+        user_data_form.save()
+        user_form.save()
+        return render(request, account_success, {
+            'user_form': user_form,
+            'user_data_form': user_data_form,
+        })
 
-        User.first_name = first_name
-        User.last_name = last_name
-        user_data.gender = gender
-        user_data.country = country
-        user_data.date_of_birth = date_of_birth
-        user_data.about = about
+    return render(request, account_management, {
+        'user_form': user_form,
+        'user_data_form': user_data_form,
+    })
 
-        if form.is_valid():
-            form.save()
-            return render(request, template_acc_success_name)
-        else:
-            return render(request, template_acc_error_name)
-
-    else:
-        return render(request, template_acc_error_name)
-
+# Show user data on profile
 def fetch_user_data(request):
     data = user_data.objects.all()
     user = User.objects.all()
     context = {'users': users, 'data': data}
-    return render(request, template_acc_name, context)
+    return render(request, account_management, context)
+
+"""work in progress below this line"""
+
+# Load forms and account management page, used in register and authview(log in)
+@login_required
+def accmanage_regauth(request):
+    user_form = UserForm(instance=request.user)
+    user_data_form = UserDataForm(instance=request.user.user_data)
+
+    context = {'user_form': user_form, 'user_data_form': user_data_form}
+    return render(request, account_management, context)
+
+# Create new user
+def register(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    password_repeat = request.POST.get('password_repeat', '')
+    email = request.POST.get('email', '')
+
+    if request.user.is_authenticated():
+        return redirect('Management')
+    else:
+        if not User.objects.filter(username=username).exists() and username != "":
+            user = User(username=username)
+            user.email = email
+            if password == password_repeat and password != "":
+                user.set_password(password)
+            else:
+                return render(request, logreg_error)
+            user.is_active = True
+            user.save()
+            userdata = user_data(user=user)
+            userdata.save()
+            return render(request, logreg_success)
+        else:
+            return render(request, logreg_error)
+
+# Log in for registered users
+def authview(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username = username, password = password)
+    if user:
+        auth.login(request, user)
+        return redirect('Management')
+    else:
+        return render(request, logreg_error)
